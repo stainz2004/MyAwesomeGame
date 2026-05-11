320def5245d0c11be13fd27c3c03c035074b7030
stainz2004 <SanderR999@gmail.com>
2026-05-11 13:13:41 +0300

    Setup done!

---
+++ game.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Pygame Setup Test")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((30, 30, 30))
+    pygame.display.update()
+
+pygame.quit()
