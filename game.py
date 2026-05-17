00e8e22451da122ef0b6b8ae352eb145721079c1
SanderR <SanderR999@gmail.com>
2026-05-17 17:56:21 +0300

    New changes :D

--- game.py
+++ game.py
@@ -2,16 +2,16 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Pygame Setup Test")
+screen = pygame.display.set_mode((1800, 1600))
 
 running = True
 
 while running:
+
+    # THis is for handling the shutdown :D
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
     screen.fill((30, 30, 30))
     pygame.display.update()
 
